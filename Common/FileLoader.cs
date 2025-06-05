27bbe8f7e7c613fc704f6d008ea04662e9c7fb60
maple1337 <madplenert@gmail.com>
2025-06-05 23:20:56 +0200

    feat: added Commons folder and FileLoader class to read and process input

---
+++ Common/FileLoader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Common
+{
+    public static class FileLoader
+    {
+        public static List<T> LoadFile<T>(string path, Func<string, T> converter)
+        {
+            return File.ReadAllLines(path)
+                        .Select(converter)
+                        .ToList();
+        }
+    }
+}
+
