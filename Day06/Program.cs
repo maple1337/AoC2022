29de203bfc29e9cb6393712e725cd2262b8421b7
maple1337 <madplenert@gmail.com>
2025-06-15 21:52:51 +0200

    Add: day06 program.cs

---
+++ Day06/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Day06;
+
+public static class Program
+{
+    private const string InputFile = "..//..//..//input.txt";
+    public static void Main()
+    {
+        Console.WriteLine("Result Part1: " + Part1());
+        Console.ReadLine();
+        Console.WriteLine("Result Part2: " + Part2());
+        Console.ReadLine();
+    }
+
+    public static int Part1()
+    {
+        DataProcessorPart1 dataProcessor = new DataProcessorPart1(InputFile);
+        return dataProcessor.SolvePuzzle();       
+    }
+    
+    public static int Part2()
+    {
+        DataProcessorPart2 dataProcessor = new DataProcessorPart2(InputFile);
+        return dataProcessor.SolvePuzzle();       
+    }
+    
+    
+}
+
